b2f858f67fdeec034fe8486e0ac64d2ddb4f7cde
zhaotianjing <tjzhao@ucdavis.edu>
2019-05-29 11:26:40 -0700

    Update eigen.jl

--- eigen.jl
+++ eigen.jl
@@ -16,3 +16,12 @@ XX2 = copy(XX)
 XX3 = copy(XX)
 @time F3 = LAPACK.syev!('N', 'U', XX3) #!! will change XX3 inplace
 @show F3[end]
+
+
+#1. no need to Symmetric(XX2), because Julia will detect whether it is Symmetric.
+#2. eigmax: first do eigvals(), then return the maximum
+#3. both eigen!() and eigvals!() use BLAS function LAPACK.sygvd! if input is symmetric.
+#4. eigen!() is slower than eigvals!(). Why?
+
+#5. eigs() in Arpack.jl is faster than eigvals() because eigs() use Arnoldi iterative methods for top eigen values, but eigvals() use LAPACK (QR algorithm). QR algotirhm will return all the eigen values.
+#http://hua-zhou.github.io/teaching/biostatm280-2017spring/slides/16-eigsvd/eigsvd.html#Lanczos/Arnoldi-iterative-method-for-top-eigen-pairs
